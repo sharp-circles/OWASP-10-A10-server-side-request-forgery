383fb3045e8062624c4e395e54709f68ec7f5e82
sharp-circles <sharpcirclesorg@gmail.com>
2025-12-21 16:57:03 +0100

    Template uploaded

---
+++ WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace OWASP_10_A10_server_side_request_forgery
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
